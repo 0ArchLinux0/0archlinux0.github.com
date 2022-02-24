a590458de2ddde96457c2266b1fc9c0ead804b17
0archlinux0 <bluebluerize900@gmail.com>
2022-02-24 15:50:07 +0900

    boj

--- main.cpp
+++ main.cpp
@@ -1,66 +1,62 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-// #include<bits/stdc++.h>
+#include <queue>
 using namespace std;
 typedef long long ll;
 const int INF = 987654321;
-int n, m, ans = 0;
-int map[500][500];
-bool visit[500][500];
-int di[] = { 1, 0, -1, 0 };
-int dj[] = { 0, 1, 0, -1 };
+int n, m, ci = 0, cj = 0, k, tailI = 0, tailJ = 0;
+bool map[100][100];
+int visit[100][100];
+int di[4] = {-1, 0, 1, 0};
+int dj[4] = {0, 1, 0, -1};
+int dir = 1;
 
-void dfs(int ii, int jj, int cnt, int sum) {
-  if(cnt == 4) {
-    ans = max(ans, sum);
-    return;
-  }
-  for(int i = 0; i < 4; i++) {
-    int ai = ii + di[i], aj = jj + dj[i];
-    if(ai < 0 || aj < 0 || ai >= n || aj >= m) continue;
-    if(visit[ai][aj]) continue;
-    visit[ai][aj] = true;
-    dfs(ai, aj, cnt + 1, sum + map[ai][aj]);
-    visit[ai][aj] = false;
-  }  
-}
+struct command {
+  int time;
+  bool clock;
+  command(int t, bool c): time(t), clock(c) {}
+};
+queue<command> q;
+queue<pair<int, int>> snake;
 
-void checkfk() {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      if(i > 0 && j < m - 1 && j > 0) {
-        ans = max(ans, map[i][j] + map[i][j+1] + map[i][j-1] + map[i-1][j]);
-      }
-      if(i > 0 && i < n - 1 && j < m - 1) {
-        ans = max(ans, map[i][j] + map[i][j+1] + map[i+1][j] + map[i-1][j]);
-      }
-      if(i < n - 1 && j < m - 1 && j > 0) {
-        ans = max(ans, map[i][j] + map[i][j+1] + map[i][j-1] + map[i+1][j]);
-      }
-      if(i > 0 && i < n - 1 && j > 0) {
-        ans = max(ans, map[i][j] + map[i][j-1] + map[i+1][j] + map[i-1][j]);
-      }
+int exec() {
+  int t = 1;
+  snake.push(make_pair(0, 0));
+  visit[0][0] = true;
+  while(true) {
+    if(!q.empty() && q.front().time == t - 1) {
+      if(q.front().clock && ++dir == 4) dir = 0;
+      if(!q.front().clock && --dir == -1) dir = 3;
+      q.pop();
     }
+    ci += di[dir], cj += dj[dir];
+    if(ci < 0 || cj < 0 || ci >= n || cj >= n || visit[ci][cj]) break;
+    visit[ci][cj] = true;
+    snake.push(make_pair(ci, cj));
+    if(!map[ci][cj]) {
+      pair<int, int> p = snake.front();
+      snake.pop();
+      visit[p.first][p.second] = false;
+    } else map[ci][cj] = false;
+    t++;
   }
+  return t;
 }
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0); cout.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) cin >> map[i][j];
+  cin >> n >> k;
+  for(int i = 0; i < k; i++) {
+    int a, b; cin >> a >> b;
+    map[a-1][b-1] = true;
   }
-
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      visit[i][j] = true;
-      dfs(i, j, 1, map[i][j]);
-      visit[i][j] = false;
-    }
+  int l; cin >> l;
+  for(int i = 0; i < l; i++) {
+    int x; char c; cin >> x >> c;
+    q.push(command(x, c == 'D'));
   }
-  checkfk();
-  cout << ans;
+  cout << exec();
   return 0;
 }
