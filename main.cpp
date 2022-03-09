b2bf388d7214625aed8a01da43bb18155e60fb5c
0archlinux0 <bluebluerize900@gmail.com>
2022-03-10 01:06:10 +0900

    boj

--- main.cpp
+++ main.cpp
@@ -21,26 +21,14 @@ using namespace std;
 const int MAX = 2002;
 const int INF = 987654321;
 
-int n;
-vector<pair<int,int>> points;
-
-bool ccw(pair<int, int> line[2], pair<int,int> p) {
-  int dx1 = line[1].first - line[0].first, dy1 = line[1].second - line[0].second;
-  int dx2 = p.first - line[0].first, dy2 = p.second - line[0].second;
-  ll result = (ll)dx1*dy2 - (ll)dy1*dx2;
-  return result > 0;
+struct line {
+  pair<int, int>
 }
+
+int n;
+vector<line> line;
+
 int main() {
   FASTIO;
-  pair<int, int> l1[2], l2[2];
-  cin >> l1[0].first >> l1[0].second >> l1[1].first >> l1[1].second >> l2[0].first >> l2[0].second >> l2[1].first >> l2[1].second;
-  bool meet = true;
-  bool c1 = ccw(l1, l2[0]), c2 = ccw(l1, l2[1]);
-  if(c1 == c2) meet = false;
-  if(meet) {
-    c1 = ccw(l2, l1[0]), c2 = ccw(l2, l1[1]);
-    if(c1 == c2) meet = false;
-  }
-  cout << (meet ? 1 : 0);
-  return 0;
+  cin >> n;
 }
