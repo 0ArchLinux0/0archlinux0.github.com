0e65a9fe1a7927bd0652ec5bd92fc27c7dc55702
0archlinux0 <bluebluerize900@gmail.com>
2022-03-11 22:44:52 +0900

    boj

--- main.cpp
+++ main.cpp
@@ -1,49 +1,53 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <cstring>
-#include <stack>
-#include <cmath>
-// #include <bits/stdc++.h>
-using namespace std;
-#define FASTIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define command_param int argc, char *argv[]
-#define for0(i, n) for(int i = 0; i < n; i++)
-#define for1(i, n) for(int i = 1; i < n; i++)
-#define fori(s, e) for(int i = s; i < e; i++)
-#define ll long long
-#define endl "\n"
-#define nulls '\0'
-#define dkdk " "
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <map>
+// #include <set>
+// #include <queue>
+// #include <cstring>
+// #include <stack>
+// #include <cmath>
+// // #include <bits/stdc++.h>
+// using namespace std;
+// #define FASTIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+// #define command_param int argc, char *argv[]
+// #define for0(i, n) for(int i = 0; i < n; i++)
+// #define for1(i, n) for(int i = 1; i < n; i++)
+// #define fori(s, e) for(int i = s; i < e; i++)
+// #define ll long long
+// #define endl "\n"
+// #define nulls '\0'
+// #define dkdk " "
 
-int hmap[500][500];
-ll dp[500][500];
-bool visit[500][500];
-int di[4] = { -1, 0, 1, 0 };
-int dj[4] = { 0, 1, 0, -1 };
-int n, m;
+// int n, m, N;
+// // vector<int> nvec;
+// vector<pair<int, int>> nvec;
+// vector<vector<int>> seg;
 
-ll dfs(int ci, int cj) {
-  if(dp[ci][cj] != 0) return dp[ci][cj];
-  if(ci == n-1 && cj == m-1) return dp[ci][cj] = 1;
-  for0(k, 4) {
-    int ai = ci + di[k], aj = cj + dj[k];
-    if(ai < 0 || aj < 0 || ai >= n || aj >= m || hmap[ci][cj] <= hmap[ai][aj]) continue;
-    if(dfs(ai, aj) == -1) continue;
-    dp[ci][cj] += dfs(ai, aj);
-  }
-  if(dp[ci][cj] == 0) dp[ci][cj] = -1;
-  return dp[ci][cj];
-}
 
-int main() {
-  FASTIO;
-  cin >> n >> m;
-  for0(i, n) for0(j, m) cin >> hmap[i][j];
-  int cnt = 0;
-  cout << (dfs(0, 0) == -1 ? 0 : dfs(0, 0));
-  return 0;
-}
+// int main() {
+//   FASTIO;
+//   cin >> n >> m; 
+//   // N = 
+//   nvec.resize(n); 
+//   // seg.resize
+//   for0(i, n) { int in; cin >> in; nvec[i] = { in, i }; };
+//   sort(nvec.begin(), nvec.end());
+//   // init();
+//   for0(i, m) {
+//     int a, b, c; cin >> a >> b >> c;
+//     a--, b--;
+//     // cout << query(a, b, c) << endl;
+//     int num, rank = 0;
+//     for0(j, n) {
+//       pair<int,int> p = nvec[j];
+//       if(a <= p.second && p.second <= b) rank++;
+//       if(rank == c) {
+//         num = p.first;
+//         break;
+//       }
+//     }
+//     cout << num << endl;
+//   }
+//   return 0;
+// }
