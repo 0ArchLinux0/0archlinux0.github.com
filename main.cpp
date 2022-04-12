af6a9b05e91b7cec1867300dfbc84375a0db6c34
0archlinux0 <bluebluerize900@gmail.com>
2022-04-13 02:26:04 +0900

    boj

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 #include <sstream>
 // #include <bits/stdc++.h>
 using namespace std;
+#ifdef ONLINE_JUDGE
+  #define NDEBUG
+#endif
 #define FASTIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define command_param int argc, char *argv[]
 #define for0(i, n) for(int i = 0; i < n; i++)
@@ -26,80 +29,45 @@ using namespace std;
 #define endl "\n"
 #define nulls '\0'
 #define dkdk " "
+
 const int INF = 987654321;
 const ll INFLL = 1e13;
 // const int mod = (int)1e9+7;
-const int mod = 32768;
-const int N = 802;
+const int mod = 10007;
+const int N = 1001;
 const int st = 50;
 // const int N = 1000000;
-int n, m, t, k, start = -1, ans = 0;
-// int adj[1001][1001];
-vector<vector<int>> adj;
+int n, m, t, k;
 
-struct Edge{
-  int u, v;
-  int cnt;
-  Edge(int _u, int _v, int _c): u(_u), v(_v), cnt(_c) {};
-};
-
-vector<Edge> edges;
-
-// void dfs(int cur) {
-//   while(adj[cur].size()) {
-//     int eidx = adj[cur].back();
-//     if(edges[eidx].cnt) {
-//       while(edges[eidx].cnt > 0) {
-//         edges[eidx].cnt--;
-//         dfs(edges[eidx].u + edges[eidx].v - cur);
-//       }
-//     }
-//     else adj[cur].pop_back();
-//   }
-//   cout << cur + 1 << dkdk;
-// }
-
-void dfs(int cur) {
-  while(adj[cur].size()) {
-    int eidx = adj[cur].back();
-    if(edges[eidx].cnt) {
-      edges[eidx].cnt--;
-      dfs(edges[eidx].u + edges[eidx].v - cur);
-    }
-    else adj[cur].pop_back();
+int rev(int n) {
+  int exp = mod-2, mul = n, ans = 1;
+  while(exp) {
+    if(exp & 1) ans = (ans * mul) % mod;
+    mul = (mul * mul) % mod;
+    exp >>= 1;
   }
-  cout << cur << dkdk;
+  return ans;
 }
 
 void solve() {
-  adj.resize(n+1);
+  int v = 1, fk, fnk;
+  if(k == 0) fk = 1;
+  if(n-k == 0) fnk = 1;
   for1(i, n+1) {
-    for1(j, n+1) {
-      if(i == j) {
-        edges.push_back(Edge(i, i, 1));
-        int eidx = edges.size() - 1;
-        adj[i].push_back(eidx);
-      }
-      if(i < j) {
-        edges.push_back(Edge(i, j, 1));
-        edges.push_back(Edge(j, i, 1));
-        int eidx = edges.size() - 2;
-        adj[i].push_back(eidx);
-        adj[j].push_back(eidx);
-        eidx++;
-        adj[i].push_back(eidx);
-        adj[j].push_back(eidx);
-      }
-    }
+    v = (v * i) % mod;
+    if(i == k) fk = v;
+    if(i == n-k) fnk = v;
   }
-  cout << (n*n + 1) << endl;
-  dfs(1);
+  cout << v*rev(fk*fnk%mod)%mod;
 }
 
 int main() {
-  FASTIO; 
-  // freopen("input.txt", "r", stdin);
-  cin >> n;
-  // adj.resize(n);
+  #ifdef NDEBUG 
+    FASTIO;
+  #else 
+    freopen("input.txt", "r", stdin);
+  #endif
+  cin >> n >> k;
   solve();
 }
+
