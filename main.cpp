e78572b046e9a84652c83c699808bd534cdabae7
0archlinux0 <bluebluerize900@gmail.com>
2022-04-17 05:55:34 +0900

    analysis

--- main.cpp
+++ main.cpp
@@ -19,44 +19,26 @@ using namespace std;
 
 const int INF = 987654321;
 const ll INFLL = 1e13;
-const int mod = 998244353;
+const int mod = (int)1e9;
 const int st = 50;
 const int N = 5e3 + 1;
 int n, m, k, t = 1, pn, mn, glen, ans;
 string s;
-ll dp[52][2502];
+ll dp[201][201];
 
-
-int get(int idx, int ub) {
-  if(dp[idx][ub] != -1) return dp[idx][ub];
-  if(idx == n-1) {
-    if(ub < 1) return 0;
-    else return dp[idx][ub] = min(m, ub);
-  } 
-  dp[idx][ub]++;
-  for1(i, min(ub,m)+1) {
-    dp[idx][ub] = (dp[idx][ub]+get(idx+1, ub - i));
-  }
-  dp[idx][ub] %= mod;
-  return dp[idx][ub];
+int get(int left, int target) {
+  if(!left) return !target;
+  int &ref = dp[left][target];
+  if(ref != -1) return ref;
+  for0(i, min(target, n)+1) ref += get(left-1, target-i);
+  ref++; ref%=mod;
+  return ref;
 }
 
 int main() {
   FASTIO;
+  cin >> n >> k;
   memset(dp, -1, sizeof(dp));
-  cin >> n >> m >> k;
-  // dp[0][0] = 1;
-  // for(int i = 0; i < n; i++) {
-  //   for(int j = 0; j < k; j++) {
-  //     // for(int v = 1; v <= min(m, k - j); v++) dp[i+1][j + v] = (dp[i+1][j+v] + dp[i][j]) % mod;
-  //     for(int v = 1; v <= m; v++) {
-  //       if(j + v <= k) dp[i+1][j + v] = (dp[i+1][j+v] + dp[i][j]) % mod;
-  //     }
-  //   }
-  // }
-  // ll ans = 0;
-  // for1(i, k+1) ans += dp[n][i];
-  // cout << ans%mod;
-  cout << get(0, k) << endl;
+  cout << get(k, n);
 }
 
